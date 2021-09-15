c999e2b5535acb701017af21c96f2f1dec7fd098
Ericsson Budhilaw <budhilaw@Ericssons-MacBook-Air.local>
2021-09-15 17:44:03 +0700

    jwt auth, validate token

--- auth/service.go
+++ auth/service.go
@@ -1,16 +1,20 @@
 package auth
 
 import (
+	"errors"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/spf13/viper"
 )
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 type jwtService struct {
 }
+
+var SecretKey = []byte(viper.GetString(`secret.key`))
 
 func NewService() *jwtService {
 	return &jwtService{}
@@ -22,10 +26,26 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
 
-	signedToken, err := token.SignedString([]byte(viper.GetString(`secret.key`)))
+	signedToken, err := token.SignedString(SecretKey)
 	if err != nil {
 		return signedToken, err
 	}
 
 	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("Invalid token")
+		}
+
+		return []byte(SecretKey), nil
+	})
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
 }
