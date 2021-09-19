65ddbb2b364af2354fccd8e77e6ddbc36be59f71
Ericsson Budhilaw <budhilaw@Ericssons-MacBook-Air.local>
2021-09-19 14:01:16 +0700

    add payment notification midtrans

--- main.go
+++ main.go
@@ -86,6 +86,7 @@ func main() {
 	api.GET("/campaigns/:id/transactions", authMiddleware(authService, userService), transactionHandler.GetCampaignTransactions)
 	api.GET("/transactions", authMiddleware(authService, userService), transactionHandler.GetUserTransactions)
 	api.POST("/transactions", authMiddleware(authService, userService), transactionHandler.CreateTransaction)
+	api.POST("/transactions/notifications", transactionHandler.GetNotification)
 
 	router.Run(viper.GetString("server.address"))
 }
